d5587f334e03c670755b11aad0d949b4dedc8fe4
Alexey Tsvetkov <avtsvetkov@sberdevices.ru>
2024-11-21 20:58:43 +0300

    use map

--- src/my_allocator.cpp
+++ src/my_allocator.cpp
@@ -57,21 +57,20 @@ struct my_allocator {
 };
 
 int main() {
-    auto v = std::vector<int, my_allocator<int, 20>>{};
-    v.reserve(5);
-    for (int i = 0; i < 6; ++i) {
-        v.emplace_back(i);
-        std::cout << std::endl;
-    }
-
     /*
-auto m = std::map<int, float, std::less<int>, my_allocator<std::pair<const int, float>>>{};
-
-for (int i = 0; i < 1; ++i) {
-    m[i] = static_cast<float>(i);
-}
-
+        auto v = std::vector<int, my_allocator<int, 20>>{};
+        v.reserve(5);
+        for (int i = 0; i < 6; ++i) {
+            v.emplace_back(i);
+            std::cout << std::endl;
+        }
     */
+
+    auto m = std::map<int, float, std::less<int>, my_allocator<std::pair<const int, float>, 100>>{};
+
+    for (int i = 0; i < 3; ++i) {
+        m[i] = static_cast<float>(i);
+    }
 
     return 0;
 }
